ed2a71c8f030689bf9e339678186e0e39868e5cc
jonggyujang0123 <jgjang0123@gmail.com>
2023-11-03 16:32:51 +0000

    initial

--- PatAtt_v4/setup.py
+++ PatAtt_v4/setup.py
@@ -1,11 +1,11 @@
 from setuptools import find_packages, setup
 
 setup(name='PatchMIA',
-        version='0.1',
+        version='0.21',
         description='Unofficial implementation ',
         url='https://github.com/jonggyujang0123/Transformer-NLP',
         author='jonggyujang0123',
         author_email='jgjang0123@gmail.com',
-        packages=find_packages(),
-        #  packages=  ['models','utils', 'tools', 'datasets' ],
+        #  packages=find_packages(),
+        packages=  ['models','utils', 'tools', 'datasets' ],
         python_requires = '>=3.7')
